7c6bc0bbcdc4283ce3b2ecd243e146080a580ab7
Mark <bortmarc8@gmail.com>
2020-10-06 13:06:50 +0200

    Creación de los modelos

---
+++ PlaceMyBet_1/PlaceMyBet_1/Models/Apuestas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlaceMyBet_1.Models
+{
+    public class Apuestas
+    {
+        public int apuesta_id { get; set; }
+        public int mercado_ref { get; set; }
+        public string usuario_ref { get; set; }
+        public int dinero { get; set; }
+
+    }
+}
---
+++ PlaceMyBet_1/PlaceMyBet_1/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlaceMyBet_1.Models
+{
+    public class Usuarios
+    {
+        public string email_id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string edad { get; set; }
+
+    }
+}
